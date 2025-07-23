b6bf0aef5f1699ffe584e5e5d5d0331bf42731b2
Bartosz Biegalski <bartoszbiegalskiwork@protonmail.com>
2025-07-23 12:09:58 +0200

    created new branch

---
+++ gmlserializer/README.md
@@ -0,0 +1,9 @@
+# gmlserializer
+
+Submodule responsible for importing, reading config files with gml structures, parsing and returning
+C++ objects.
+
+# Used libraries
+* libxml 2.6
+* nlohmann::json 
+
---
+++ gmlserializer/build/CMakeFiles/gmlserializer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmlserializer.dir/src/main.cpp.o"
+  "CMakeFiles/gmlserializer.dir/src/main.cpp.o.d"
+  "gmlserializer"
+  "gmlserializer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmlserializer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
