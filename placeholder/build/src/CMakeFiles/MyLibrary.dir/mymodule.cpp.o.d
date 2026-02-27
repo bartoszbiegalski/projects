4b93c62409dd5da9ad57e02e8b5ea1685f9be21b
bartoszbiegalski <147548221+bartoszbiegalski@users.noreply.github.com>
2026-02-27 19:06:44 +0100

    Delete placeholder directory

--- placeholder/build/src/CMakeFiles/MyLibrary.dir/mymodule.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/MyLibrary.dir/mymodule.cpp.o: \
- /home/bartek/Documents/projects/placeholder/src/mymodule.cpp \
- /usr/include/stdc-predef.h \
- /home/bartek/Documents/projects/placeholder/include/mymodule.h
